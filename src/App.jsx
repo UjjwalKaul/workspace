acb079251816fdd16f78ff85afad112b1510ff8b
Ujjwal Kaul <ujjwal171@outlook.com>
2023-11-16 19:00:15 +0000

    Sandbox snapshot commit

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,29 @@ function App() {
   function handleStartAddProject(){
     setProjectsState((prevState)=>{return {...prevState,selectedProjectId:null}})
   }
+  
+  function handleAddProject(projectData){
+    setProjectsState((prevState)=>{
+      const newProject={
+        ...projectData,
+        id:Math.random()
+      };  
+      return {...prevState,projects:[...prevState.projects,newProject]}
+    })
+  }
+  
+  let content;
+  if(projectsState.selectedProjectId===null){
+    content=<NewProject onAdd={handleAddProject}/>
+  }
+  else if(projectsState.selectedProjectId===undefined){
+    content=<NoProjectSelected onStartAddProject={handleStartAddProject}/>
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8" >
       <ProjectSidebar onStartAddProject={handleStartAddProject}/>
-      <NoProjectSelected onStartAddProject={handleStartAddProject}/>
+      {content}
     </main>
   );
 }
--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,15 +1,30 @@
+import { useRef } from "react";
 import Input from "./Input";
 
-export default function NewProject(){
+export default function NewProject({onAdd}){
+    const title=useRef();
+    const desc=useRef();
+    const dueDate=useRef();
+
+    function handleSave(){
+        const enteredTitle=title.current.value;
+        const enteredDesc=desc.current.value;
+        const enteredDueDate=dueDate.current.value;
+        onAdd({
+            title:enteredTitle,
+            desc:enteredDesc,
+            dueDate:enteredDueDate
+        })
+    }
     return <div className="w-[35rem] mt-16">
         <menu className="flex items-center justify-end gap-4 my-4">
             <li><button className="text-stone-800 hover:text-stone-950">Cancel</button></li>
-            <li><button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button></li>
+            <li><button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950" onClick={handleSave}>Save</button></li>
         </menu>
         <div>
-            <Input label="Title" textarea={false} />
-            <Input label="Description" textarea={true} />
-            <Input label="Date" textarea={false} />
+            <Input label="Title" type="text" textarea={false} ref={title} />
+            <Input label="Description" textarea={true} ref={desc} />
+            <Input type="date" label="Date" textarea={false} ref={dueDate}/>
         </div>
     </div>
 }
