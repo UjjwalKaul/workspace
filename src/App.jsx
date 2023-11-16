f8486f831a8ea31748d821b45ff801397f555dff
Ujjwal Kaul <ujjwal171@outlook.com>
2023-11-16 14:25:42 +0000

    Sandbox snapshot commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import NewProject from "../src/components/NewProject"
 
 function App() {
   return (
-    <main className="h-screen my-8">
+    <main className="h-screen my-8 flex gap-8" >
       <ProjectSidebar/>
       <NewProject/>
     </main>
