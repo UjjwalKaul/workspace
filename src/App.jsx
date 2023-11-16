263685139cea64ca86b6d0bab5a9d47fbf1188a8
Ujjwal Kaul <ujjwal171@outlook.com>
2023-11-16 18:50:51 +0000

    Added Button component and No Project Selected case

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import ProjectSidebar from "./components/ProjectSidebar";
 import NewProject from "../src/components/NewProject"
+import NoProjectSelected from "./components/NoProjectSelected";
+import { useState } from "react";
 
 function App() {
+  const[projectsState,setProjectsState]=useState({
+    selectedProjectId:undefined,
+    projects:[]
+  });
   return (
     <main className="h-screen my-8 flex gap-8" >
       <ProjectSidebar/>
-      <NewProject/>
+      <NoProjectSelected/>
     </main>
   );
 }
