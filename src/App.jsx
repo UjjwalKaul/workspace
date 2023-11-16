d8aa4444cf48cddb0ae74d2cd30001230b75d1d5
Ujjwal Kaul <ujjwal171@outlook.com>
2023-11-16 14:03:58 +0000

    Sandbox snapshot commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import ProjectSidebar from "./components/ProjectSidebar";
+import NewProject from "../src/components/NewProject"
 
 function App() {
   return (
     <main className="h-screen my-8">
       <ProjectSidebar/>
+      <NewProject/>
     </main>
   );
 }
